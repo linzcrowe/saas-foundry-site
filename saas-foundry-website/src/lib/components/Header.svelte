07aa21171b8ca47387a54cec3af4fcee324e3dee
linzcrowe <lindsey@onebigocean.com.au>
2024-06-27 21:13:26 +1000

    done

--- saas-foundry-website/src/lib/components/Header.svelte
+++ saas-foundry-website/src/lib/components/Header.svelte
@@ -11,15 +11,17 @@
 </script>
 
 <header class="px-4 lg:px-6 h-14 flex items-center bg-gray-100">
-	<a href="/" class="flex items-center justify-center">
-		<MountainIcon className="h-6 w-6" />
-		<span class="sr-only">{siteName}</span>
-	</a>
-	<nav class="ml-auto flex gap-4 sm:gap-6">
-		{#each navLinks as link}
-			<a href={link.href} class="text-sm font-medium hover:underline underline-offset-4">
-				{link.text}
-			</a>
-		{/each}
-	</nav>
+	<div class="container mx-auto flex items-center justify-between w-full">
+		<a href="/" class="flex items-center">
+			<MountainIcon className="h-6 w-6" />
+			<span class="sr-only">{siteName}</span>
+		</a>
+		<nav class="flex gap-4 sm:gap-6">
+			{#each navLinks as link}
+				<a href={link.href} class="text-sm font-medium hover:underline underline-offset-4">
+					{link.text}
+				</a>
+			{/each}
+		</nav>
+	</div>
 </header>
