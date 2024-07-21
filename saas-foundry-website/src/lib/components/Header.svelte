880be67d3a84e9d01b68eb4a71aa2b17078ad0d4
Lindsey Crowe <linzcrowe@gmail.com>
2024-07-21 22:16:11 +1000

    fix navbar

--- saas-foundry-website/src/lib/components/Header.svelte
+++ saas-foundry-website/src/lib/components/Header.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-	import { MountainIcon } from '$lib/Icons';
+	import Logo from '$lib/assets/header-logo.png';
 	import type { NavLink } from '$lib/types';
-
-	export let siteName: string = 'SaaS Foundry';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+	} from 'flowbite-svelte';
 
 	export let navLinks: NavLink[] = [
 		{ href: '#services', text: 'Services' },
@@ -10,18 +15,26 @@
 	];
 </script>
 
-<header class="px-4 lg:px-6 h-14 flex items-center bg-gray-100 sticky top-0 z-50 bg-white">
-	<div class="container mx-auto flex items-center justify-between w-full">
-		<a href="/" class="flex items-center">
-			<MountainIcon className="h-6 w-6" />
-			<span class="sr-only">{siteName}</span>
-		</a>
-		<nav class="flex gap-4 sm:gap-6">
-			{#each navLinks as link}
-				<a href={link.href} class="text-sm font-medium hover:underline underline-offset-4">
-					{link.text}
-				</a>
-			{/each}
-		</nav>
+<header>
+	<div class="relative px-8">
+		<Navbar class="fixed start-0 top-0 z-20 w-full border-b px-2 py-2.5 sm:px-4">
+			<NavBrand href="/">
+				<img src={Logo} class="me-3 h-6 sm:h-9" alt="SaaS Foundry Logo" />
+			</NavBrand>
+			<NavHamburger />
+			<NavUl>
+				{#each navLinks as link}
+					<NavLi href={link.href}>{link.text}</NavLi>
+				{/each}
+			</NavUl>
+		</Navbar>
 	</div>
 </header>
+
+<style>
+	.MenuIcon {
+		display: block; /* Make it a block element for sizing */
+		width: 24px;
+		height: 24px;
+	}
+</style>
