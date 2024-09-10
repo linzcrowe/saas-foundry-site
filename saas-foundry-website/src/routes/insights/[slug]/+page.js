6703913bf30a9aa8d6fb92b3e90341988956c60d
Lindsey Crowe <linzcrowe@gmail.com>
2024-09-10 15:55:49 +1000

    temporarily remove pre-render on slugs to fix build

--- saas-foundry-website/src/routes/insights/[slug]/+page.js
+++ saas-foundry-website/src/routes/insights/[slug]/+page.js
@@ -1,7 +1,7 @@
 import { dev } from '$app/environment';
 
 export const csr = dev;
-export const prerender = true;
+export const prerender = false; // temp workaround, pre-render is failing. TODO fix
 
 export async function load({ params, fetch }) {
   const { slug } = params;
